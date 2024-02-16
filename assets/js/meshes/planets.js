662468a5355b6e5dcc6bd015b251acc0a54ee17e
Mitko Milev <milevk2@gmail.com>
2024-02-16 23:59:21 +0200

    planets parameters adjusted to work with dt

--- assets/js/meshes/planets.js
+++ assets/js/meshes/planets.js
@@ -1,29 +1,30 @@
 import { FrontSide, IcosahedronGeometry, MeshLambertMaterial } from "three";
 import { OrbitingPlanet, RingPlanet } from "../SpaceObjectBuilder.js";
 import { attach_name_labels } from "./fonts.js";
+import { rotationPivotConfig, rotationSelfConfig } from "../animation/rotationMultiplier.js";
 
-
-
-
-const mercury = new OrbitingPlanet('Mercury', IcosahedronGeometry, [3, 4], MeshLambertMaterial, 'mercury.png', FrontSide, false, [10, 0, 25], 1, 2.5, 1.5);
-const venus = new OrbitingPlanet('Venus', IcosahedronGeometry, [4, 4], MeshLambertMaterial, 'venus.png', FrontSide, false, [4, 15, 40], 1, 2.5, 1);
-const earth = new OrbitingPlanet('Earth', IcosahedronGeometry, [7, 4], MeshLambertMaterial, 'earth.png', FrontSide, false, [15, -15, 75], 1, 2.5, 0.6);
-const moon = new OrbitingPlanet('Moon', IcosahedronGeometry, [2, 4], MeshLambertMaterial, 'moon.png', FrontSide, false, [0, 0, 0], 1, 2.5, 10);
-const mars = new OrbitingPlanet('Mars', IcosahedronGeometry, [6, 4], MeshLambertMaterial, 'mars.png', FrontSide, false, [5, 10, 125], 1, 2.5, 0.2);
-const jupiter = new OrbitingPlanet('Jupiter', IcosahedronGeometry, [10, 4], MeshLambertMaterial, 'jupiter.png', FrontSide, false, [-40, 20, 250], 1, 2.5, 0);
-const saturn = new RingPlanet('Saturn', IcosahedronGeometry, [9, 4], MeshLambertMaterial, 'saturn.png', FrontSide, false, [45, 20, 320], 1, 2.5, -2);
-const uranus = new OrbitingPlanet('Uranus', IcosahedronGeometry, [5, 4], MeshLambertMaterial, 'uranus.png', FrontSide, false, [45, 40, 390], 1, 2.5, -3);
-const neptune = new OrbitingPlanet('Neptune', IcosahedronGeometry, [6, 4], MeshLambertMaterial, 'neptune.png', FrontSide, false, [25, -10, 420], 1, 2.5, -3.5);
-const pluto = new OrbitingPlanet('Pluto', IcosahedronGeometry, [2, 4], MeshLambertMaterial, 'pluto.png', FrontSide, false, [30, -5, 450], 1, 2.5, -4);
+const mercury = new OrbitingPlanet('Mercury', IcosahedronGeometry, [3, 4], MeshLambertMaterial, 'mercury.png', FrontSide, false, [25, 0, 25], 1, 2.5 * rotationSelfConfig, 180 * rotationPivotConfig);
+const venus = new OrbitingPlanet('Venus', IcosahedronGeometry, [4, 4], MeshLambertMaterial, 'venus.png', FrontSide, false, [75, 15, 75], 1, 2.5 * rotationSelfConfig, 160 * rotationPivotConfig);
+const earth = new OrbitingPlanet('Earth', IcosahedronGeometry, [7, 4], MeshLambertMaterial, 'earth.png', FrontSide, false, [125, -15, 125], 1, 1 * rotationSelfConfig, 130 * rotationPivotConfig);
+const moon = new OrbitingPlanet('Moon', IcosahedronGeometry, [2, 4], MeshLambertMaterial, 'moon.png', FrontSide, false, [0, 0, 0], 1, 2.5 * rotationSelfConfig, 180 * rotationPivotConfig);
+const mars = new OrbitingPlanet('Mars', IcosahedronGeometry, [6, 4], MeshLambertMaterial, 'mars.png', FrontSide, false, [175, 10, 175], 1, 2.5 * rotationSelfConfig, 100 * rotationPivotConfig);
+const jupiter = new OrbitingPlanet('Jupiter', IcosahedronGeometry, [10, 4], MeshLambertMaterial, 'jupiter.png', FrontSide, false, [225, 20, 225], 1, 2.5 * rotationSelfConfig, 80 * rotationPivotConfig);
+const saturn = new RingPlanet('Saturn', IcosahedronGeometry, [9, 4], MeshLambertMaterial, 'saturn.png', FrontSide, false, [350, 20, 350], 1, 0 * rotationSelfConfig, 60 * rotationPivotConfig);
+const uranus = new OrbitingPlanet('Uranus', IcosahedronGeometry, [5, 4], MeshLambertMaterial, 'uranus.png', FrontSide, false, [10, 40, 400], 1, 2.5 * rotationSelfConfig, 30 * rotationPivotConfig);
+const neptune = new OrbitingPlanet('Neptune', IcosahedronGeometry, [6, 4], MeshLambertMaterial, 'neptune.png', FrontSide, false, [25, -10, 420], 1, 2.5 * rotationSelfConfig, 15 * rotationPivotConfig);
+const pluto = new OrbitingPlanet('Pluto', IcosahedronGeometry, [2, 4], MeshLambertMaterial, 'pluto.png', FrontSide, false, [30, -5, 450], 1, 2.5 * rotationSelfConfig, 5 * rotationPivotConfig);
 
 //Moon is bound to Earth:
 earth.mesh.add(moon.pivot);
 moon.pivot.position.set(0, 0, 0)
 moon.mesh.position.set(11, 0, 0)
 
+/*
+attaching the name labels to the planets only after the planets array has been constructed; 
+The function returns a promise which fullfills only after all textMeshes have been created 
+*/
 const planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto];
+let isNameLabelsLoaded =  await attach_name_labels(planets); 
 
-attach_name_labels(planets); // attaching the name labels to the planets only after the planets array has been constructed;
 
-
-export { planets, moon } //exporting the moon separately so I can animate it in the animationLoop
+export { planets, moon, isNameLabelsLoaded} //exporting the moon separately so I can animate it in the animationLoop
