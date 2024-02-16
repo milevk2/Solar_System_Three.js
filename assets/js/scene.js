211c2a78f68d58815e3a762d94bf2a9f7fe05477
Mitko Milev <milevk2@gmail.com>
2024-02-17 01:00:10 +0200

    Scene setting improved. The planets pivots are no longer attached to the sun, as  they were being affected by the sun`s selfrotation speed, which lead to unexpected behaviour.

--- assets/js/scene.js
+++ assets/js/scene.js
@@ -5,7 +5,6 @@ import galaxy from "./meshes/galaxy.js";
 import {camera, cameraPivot} from "./camera.js";
 
 //-------------------SCENE PREPARATION---------------
-
 const scene = new Scene();
 
 //ambient light settings:
@@ -14,25 +13,24 @@ ambientLight.layers.set(1);
 
 //point light settings - it is in the center of the Sun, bright, unlimited distance reach and no light dispersion:
 const pointLight = new PointLight( 0xFDB813,0.5,0,0);
-pointLight.position.set(0, 0, 0)
+pointLight.position.set(0, 0, 0);
 pointLight.layers.set(1);
 
 //adding the entities to the scene:
 scene.add(ambientLight);
 scene.add(pointLight);
 scene.add(galaxy.mesh);
-
-sun.mesh.add(cameraPivot);
+scene.add(sun.mesh);
+pointLight.add(cameraPivot);
 
 //I asked chat gpt whether there is a direction vector for the position the camera is looking at so I could calculate the angle after camera position change. 
 //He showed me the below amazing function:
-camera.lookAt(sun.mesh.position)
-//All planets pivots are attached to the Sun XYZ 0,0,0:
-scene.add(sun.mesh);
+camera.lookAt(sun.mesh.position);
 
+//All planets pivots are attached to the PointLight XYZ 0,0,0:
 for (let planet of planets) {
 
-    sun.mesh.add(planet.pivot);
+    pointLight.add(planet.pivot);
 }
 
 export default scene;
