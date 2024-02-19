8d348e0e2fd80489f46dd9f26cb14e1e2f6a8e42
Mitko Milev <milevk2@gmail.com>
2024-02-19 13:53:36 +0200

    added a showNameLabels boolean flag and logic so the functionality will show the nameLabels only after cameraControls have been initialized

--- assets/js/animation/VisualEffects.js
+++ assets/js/animation/VisualEffects.js
@@ -1,10 +1,15 @@
-import { camera } from "../camera.js";
+import { camera } from "../camera/camera.js"
+
+let showNameLabels = { show: false } // the state will change to true in cameraMainFunction once we deploy the OrbitControls.
 
 function applyVisualEffect(intersected) {
 
     intersected.material.emissive.set(0x3D85C6);
-    intersected[intersected['name_label_id']].lookAt(camera.position);
-    intersected[intersected['name_label_id']].layers.set(1);
+
+        if (showNameLabels.show) {
+        intersected[intersected['name_label_id']].lookAt(camera.position);
+        intersected[intersected['name_label_id']].layers.set(1);
+    }    
 }
 
 function resetVisualEffects(spaceObjectsArray) {
@@ -13,19 +18,20 @@ function resetVisualEffects(spaceObjectsArray) {
 
         spaceObj.mesh.material.emissive.set(0x000000);
 
-        if (spaceObj.hasOwnProperty('nameMesh')){
+        if (spaceObj.hasOwnProperty('nameMesh')) {
 
             spaceObj.nameMesh.layers.set(2)
         }
     }
 }
 
-function lookAtCamera (planets) {
+function lookAtCamera(planets) {
+
+    if (showNameLabels.show == false) return
 
     for (let planet of planets) {
-
         planet.nameMesh.lookAt(camera.position);
     }
 }
 
-export {applyVisualEffect, resetVisualEffects, lookAtCamera}
+export { applyVisualEffect, resetVisualEffects, lookAtCamera, showNameLabels }
