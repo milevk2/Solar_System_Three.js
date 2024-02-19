3af5546f70fb9393f7fa2c6155287c7be0908adb
Mitko Milev <milevk2@gmail.com>
2024-02-19 14:19:29 +0200

    earth is now calling the PlanetWithSatellite class constructor and passing the moon to it

--- assets/js/meshes/planets.js
+++ assets/js/meshes/planets.js
@@ -1,12 +1,15 @@
 import { FrontSide, IcosahedronGeometry, MeshLambertMaterial } from "three";
-import { OrbitingPlanet, RingPlanet } from "../SpaceObjectBuilder.js";
+import { OrbitingPlanet, PlanetWithSatellite, RingPlanet } from "../SpaceObjectBuilder.js";
 import { attach_name_labels } from "./fonts.js";
 import { rotationPivotConfig, rotationSelfConfig } from "../animation/rotationMultiplier.js";
 
+//creating the planets with their respective parameters, passed to their respective class constructors
 const mercury = new OrbitingPlanet('Mercury', IcosahedronGeometry, [4, 4], MeshLambertMaterial, 'mercury.png', FrontSide, false, [25, 0, 60], 1, 2.5 * rotationSelfConfig, 180 * rotationPivotConfig);
 const venus = new OrbitingPlanet('Venus', IcosahedronGeometry, [5, 4], MeshLambertMaterial, 'venus.png', FrontSide, false, [75, 15, 75], 1, 2.5 * rotationSelfConfig, 160 * rotationPivotConfig);
-const earth = new OrbitingPlanet('Earth', IcosahedronGeometry, [8, 4], MeshLambertMaterial, 'earth.png', FrontSide, false, [125, -15, 125], 1, 1 * rotationSelfConfig, 130 * rotationPivotConfig);
+
+//first creating the moon and then passing the object to the earth's PlanetWithSatellite constructor
 const moon = new OrbitingPlanet('Moon', IcosahedronGeometry, [3, 4], MeshLambertMaterial, 'moon.png', FrontSide, false, [0, 0, 0], 1, 2.5 * rotationSelfConfig, 180 * rotationPivotConfig);
+const earth = new PlanetWithSatellite('Earth', IcosahedronGeometry, [8, 4], MeshLambertMaterial, 'earth.png', FrontSide, false, [125, -15, 125], 1, 1 * rotationSelfConfig, 130 * rotationPivotConfig, moon);
 const mars = new OrbitingPlanet('Mars', IcosahedronGeometry, [7, 4], MeshLambertMaterial, 'mars.png', FrontSide, false, [175, 10, 175], 1, 2.5 * rotationSelfConfig, 100 * rotationPivotConfig);
 const jupiter = new OrbitingPlanet('Jupiter', IcosahedronGeometry, [11, 4], MeshLambertMaterial, 'jupiter.png', FrontSide, false, [225, 20, 225], 1, 2.5 * rotationSelfConfig, 80 * rotationPivotConfig);
 const saturn = new RingPlanet('Saturn', IcosahedronGeometry, [10, 4], MeshLambertMaterial, 'saturn.png', FrontSide, false, [350, 20, 300], 1, 0 * rotationSelfConfig, 60 * rotationPivotConfig);
@@ -15,9 +18,9 @@ const neptune = new OrbitingPlanet('Neptune', IcosahedronGeometry, [7, 4], MeshL
 const pluto = new OrbitingPlanet('Pluto', IcosahedronGeometry, [5, 4], MeshLambertMaterial, 'pluto.png', FrontSide, false, [30, -5, 405], 1, 2.5 * rotationSelfConfig, 5 * rotationPivotConfig);
 
 //Moon is bound to Earth:
-earth.mesh.add(moon.pivot);
-moon.pivot.position.set(0, 0, 0)
-moon.mesh.position.set(11, 0, 0)
+// earth.mesh.add(moon.pivot);
+// moon.pivot.position.set(0, 0, 0)
+// moon.mesh.position.set(11, 0, 0)
 
 /*
 attaching the name labels to the planets only after the planets array has been constructed; 
