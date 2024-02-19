036a7ddeceb4655b90939de92ad08877eb7c41a0
Mitko Milev <milevk2@gmail.com>
2024-02-19 14:02:50 +0200

    integrated the new cameraMainFunction in the animation loop. Added a wheeling event on the canvas; added isWheeling and documentDeltaY variables so they can save the data from the wheeling event scope and pass it to cameraFunction in the animation loopp.

--- main.js
+++ main.js
@@ -2,18 +2,21 @@ import scene from "./assets/js/scene.js";
 import sun from "./assets/js/meshes/sun.js";
 import galaxy from "./assets/js/meshes/galaxy.js";
 import rayCast from "./assets/js/rayCast.js";
-import { pause } from "./assets/js/utility/pause_ms.js";
-import { camera, rotateCamera } from "./assets/js/camera.js"
-import planetRenderer  from "./assets/js/renderers/renderer.js";
+import { pause, pause_ms } from "./assets/js/utility/pause_ms.js";
+import { camera, cameraMainFunction, cameraAutoRotate} from "./assets/js/camera/camera.js"
+import planetRenderer from "./assets/js/renderers/renderer.js";
 import { planets, moon, nameLabelsLoaded } from "./assets/js/meshes/planets.js";
 import { lookAtCamera, resetVisualEffects } from "./assets/js/animation/VisualEffects.js";
-import {rotateSelf, rotatePivot} from "./assets/js/animation/rotation.js";
+import { rotateSelf, rotatePivot } from "./assets/js/animation/rotation.js";
 import { Clock } from "three";
+import { cameraControls } from "./assets/js/camera/controls.js";
+//import cameraControls from "./assets/js/camera/controls.js";
 
+const canvas = document.querySelector('.webgl')
 
 //pause logic on scene click:
-document.querySelector('.webgl').addEventListener("mousedown", (e) => pause.value = true);
-document.querySelector('.webgl').addEventListener("mouseup", (e) => pause.value = false);
+canvas.addEventListener("mousedown", (e) => pause.value = true);
+canvas.addEventListener("mouseup", (e) => pause.value = false);
 
 window.addEventListener(
     "resize",
@@ -24,27 +27,50 @@ window.addEventListener(
     },
 );
 
+
+let isWheeLing = false;
+let documentDeltaY = 0;
+
+canvas.addEventListener("wheel", (e) => {
+    //extracting the data to the outer scope so we can pass it to the animation loop to ensure smooth rotation;
+    documentDeltaY = e.deltaY;
+    isWheeLing = true;
+})
+
 const clock = new Clock();
 let deltaTime;
 
 const animate = () => {
     requestAnimationFrame(animate);
 
+    if (nameLabelsLoaded == undefined) return;
+
     deltaTime = clock.getDelta();
-  
+
     if (pause.value == false) {
-        
-        rotateCamera(deltaTime);
+
+        if (isWheeLing) {
+
+            cameraMainFunction(deltaTime, documentDeltaY);
+        }
+
+        if(cameraControls !== null) {
+
+            cameraControls.update();
+        }
+
+        cameraAutoRotate(deltaTime);
         rotateSelf([galaxy, moon, sun, ...planets], deltaTime);
         rotatePivot([moon, ...planets], deltaTime);
         resetVisualEffects([moon, ...planets]);
-        lookAtCamera([...planets]); 
+        lookAtCamera([...planets]);     //it is the planets nameLabels we are making look at the camera;
+        documentDeltaY = 0;             //zeroing out the deltaY, otherwise the cameraMainFunction will continue affecting the camera position
+        isWheeLing = false;             //just like the below comment - I want to be sure there won't be any side effects;
     }
     rayCast();
     planetRenderer.render(scene, camera);
 };
+animate();
 
-setTimeout(animate,1500) // waits 1.5 seconds before animation start just to make sure all the modules have loaded;
-
-
+//setTimeout(animate,1500) // waits 1.5 seconds before animation start just to make sure all the modules have loaded;
 
