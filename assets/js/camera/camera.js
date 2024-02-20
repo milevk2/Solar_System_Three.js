a560e4c134e45423583d38b914684c7d441cc685
Mitko Milev <milevk2@gmail.com>
2024-02-20 23:56:11 +0200

    added     onCameraInitialize() function which prepares the scene with the relevant settings. Texts for each planet now show on planet focus

--- assets/js/camera/camera.js
+++ assets/js/camera/camera.js
@@ -4,6 +4,7 @@ import sun from "../meshes/sun.js";
 import scene from "../scene.js";
 import { cameraControls, initiateControls } from "./controls.js";
 import { showNameLabels } from "../animation/VisualEffects.js";
+import animateText from "../animation/cssTextAnimate.js";
 
 const [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune, pluto] = planets;
 
@@ -19,8 +20,8 @@ const near = 0.1;
 const far = 3000;
 
 const camera = new PerspectiveCamera(fov, aspect, near, far);
-camera.position.z = 50;
-camera.position.x = 50;
+camera.position.z = 100;
+camera.position.x = 0;
 camera.position.y = 0;
 camera.layers.set(1);
 
@@ -64,13 +65,14 @@ function cameraAutoRotate(deltaTime) {
     if (cameraControls !== null) return // stop autoRotations on cameraControls initialization as cameraControls have their own rotation method;
     adjustRotationX();
     cameraPivot.mesh.rotateY(directionY * deltaTime);
-    cameraPivot.mesh.rotateX(directionX * deltaTime);
+    // cameraPivot.mesh.rotateX(directionX * deltaTime);
     camera.updateProjectionMatrix();
 }
 
 
 let cameraPivot = sun;
 let previousCameraPivot = null;
+let previousPlanetName = null;
 
 function updatePivot(newPivotPlanet) {
 
@@ -84,6 +86,7 @@ function updatePivot(newPivotPlanet) {
     cameraPivot.savedRotation_self = cameraPivot.rotation_self;
     cameraPivot.rotation_self = 0.1;
     previousCameraPivot = cameraPivot;
+    previousPlanetName = cameraPivot.name.toLowerCase();
 }
 
 
@@ -91,16 +94,15 @@ function cameraMainFunction(deltaTime, documentDeltaY) {
 
     const documentPosition = window.scrollY;
 
-    //if (cameraControls == null && scrollY > 13500) return;
-
     if (documentPosition > 50 && documentPosition <= 1500) {
 
         if (cameraPivot.name == 'Mercury') {
 
             rotatePivotOnScroll(mercury.mesh, documentDeltaY, deltaTime);
             return
-        }
-
+        } 
+        camera.position.z = 20;
+        animateText('mercury', previousPlanetName);
         updatePivot(mercury);
     }
     else if (documentPosition > 1500 && documentPosition <= 3000) {
@@ -110,7 +112,8 @@ function cameraMainFunction(deltaTime, documentDeltaY) {
             rotatePivotOnScroll(venus.mesh, documentDeltaY, deltaTime);
             return
         }
-
+        camera.position.z = 50;
+        animateText('venus', previousPlanetName);
         updatePivot(venus);
     }
     else if (documentPosition > 3000 && documentPosition <= 4500) {
@@ -120,7 +123,7 @@ function cameraMainFunction(deltaTime, documentDeltaY) {
             rotatePivotOnScroll(earth.mesh, documentDeltaY, deltaTime);
             return
         }
-
+        animateText('earth', previousPlanetName);
         updatePivot(earth);
     }
     else if (documentPosition > 4500 && documentPosition <= 6000) {
@@ -130,8 +133,8 @@ function cameraMainFunction(deltaTime, documentDeltaY) {
             rotatePivotOnScroll(mars.mesh, documentDeltaY, deltaTime);
             return
         }
-
-       updatePivot(mars);
+        animateText('mars', previousPlanetName);
+        updatePivot(mars);
     }
     else if (documentPosition > 6000 && documentPosition <= 7500) {
 
@@ -140,7 +143,8 @@ function cameraMainFunction(deltaTime, documentDeltaY) {
             rotatePivotOnScroll(jupiter.mesh, documentDeltaY, deltaTime);
             return
         }
-
+        camera.position.z = 100;
+        animateText('jupiter', previousPlanetName);
         updatePivot(jupiter);
     }
     else if (documentPosition > 7500 && documentPosition <= 9000) {
@@ -150,8 +154,8 @@ function cameraMainFunction(deltaTime, documentDeltaY) {
             rotatePivotOnScroll(saturn.mesh, documentDeltaY, deltaTime);
             return
         }
-
-       updatePivot(saturn);
+        animateText('saturn', previousPlanetName);
+        updatePivot(saturn);
     }
     else if (documentPosition > 9000 && documentPosition <= 10500) {
 
@@ -160,7 +164,8 @@ function cameraMainFunction(deltaTime, documentDeltaY) {
             rotatePivotOnScroll(uranus.mesh, documentDeltaY, deltaTime);
             return
         }
-
+        camera.position.z = 30;
+        animateText('uranus', previousPlanetName);
         updatePivot(uranus);
     }
     else if (documentPosition > 10500 && documentPosition <= 12000) {
@@ -170,7 +175,7 @@ function cameraMainFunction(deltaTime, documentDeltaY) {
             rotatePivotOnScroll(neptune.mesh, documentDeltaY, deltaTime);
             return
         }
-
+        animateText('neptune', previousPlanetName);
         updatePivot(neptune);
     }
     else if (documentPosition > 12000 && documentPosition <= 13000) {
@@ -179,18 +184,16 @@ function cameraMainFunction(deltaTime, documentDeltaY) {
 
             rotatePivotOnScroll(pluto.mesh, documentDeltaY, deltaTime);
             return
-        }
-
+        }   
+        
+        animateText('pluto', previousPlanetName);
         updatePivot(pluto);
     }
     else if (documentPosition > 13000) {
 
-       
         if (cameraControls !== null) return;
-        pluto.mesh.remove(camera);
-        showNameLabels.show = true;
-        console.log('camera initiated');
-        initiateControls();
+        onCameraInitialize();
+
     }
 }
 
@@ -199,6 +202,16 @@ function rotatePivotOnScroll(planet, documentDeltaY, deltaTime) {
     let multiplier = documentDeltaY > 0 ? 1 : -1;  //determines the spinning direction of the camera around it's pivot planet
     planet.rotateY(20 * multiplier * deltaTime);
     camera.updateProjectionMatrix();
+}
+
+function onCameraInitialize() {
+
+    camera.position.z = 100;
+    animateText('freeLook', previousPlanetName);
+    pluto.mesh.remove(camera);
+    showNameLabels.show = true;
+    console.log('Camera initialized!');
+    initiateControls();
 }
 
 export { camera, cameraPivot, cameraAutoRotate, cameraMainFunction };
