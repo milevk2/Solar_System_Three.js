e3c7237d53b3788fa52ae8194e14377fc870768e
Mitko Milev <milevk2@gmail.com>
2024-02-21 00:50:07 +0200

    deleted console.log

--- assets/js/animation/cssTextAnimate.js
+++ assets/js/animation/cssTextAnimate.js
@@ -3,7 +3,6 @@
 //the function is used in cameraMainFunction, they are tightly coupled I have to make the logic more abstract
 function animateText (planetId, previousPlanetId = null) {
 
-    console.log(planetId, previousPlanetId);
     document.getElementById(planetId).classList.add('animatedText')
     
     if (previousPlanetId == null) return;
